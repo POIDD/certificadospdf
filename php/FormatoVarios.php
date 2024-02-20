86a19a504bc7e486dcbf8293d7e27938c6e930f5
vanessa <dellyvanessacruz007@gmail.com>
2024-02-19 22:16:44 -0500

    plantilla pdf elemento varios

---
+++ php/FormatoVarios.php
@@ -0,0 +1,138 @@
+<?php
+
+    require('../fpdf/fpdf.php');
+
+
+    class PDF extends FPDF
+    {
+    // Cabecera de página
+    function Header()
+    {
+        // Logo
+        $this->Image('../img/logo-consejo-danes.jpg',152,8,33);
+        $this->Image('../img/mapaDRC.jpg',68,8,78);
+
+        // Arial bold 15
+        $this->SetFont('Arial','B',7);
+        $this->Ln(9);
+        // Movernos a la derecha
+        $this->Cell(69);
+        $this->Cell(60,6,utf8_decode('Consejo Dánes para Refugiados'),0,0,'L');
+        $this->SetFont('Arial','B',9);
+        $this->Ln(2);
+        $this->Cell(69,6,'San Jose del Fragua, 17 enero. de 2024',0,0,'L');
+        $this->SetFont('Arial','',7);
+        $this->Cell(90,10,utf8_decode('Calle 70 A #7-95'),0,0,'L');
+        $this->Ln(4);
+        $this->Cell(69);
+        $this->Cell(90,10,utf8_decode('110231, Bogotá'),0,0,'L');
+        $this->Ln(4);
+        $this->Cell(69);
+        $this->Cell(90,10,utf8_decode('Colombia'),0,0,'L');
+        $this->Ln(18);
+
+        
+    }
+
+    // Pie de página
+    
+    }
+
+
+
+    $pdf = new PDF();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial','B',9);
+    $pdf->SetTextColor(170, 25, 31); // Establece el color del texto a rojo
+    $pdf->SetX(73);
+    $pdf->Cell(40,10,utf8_decode('ACTA DE ENTREGA DE DOTACIÓN'));
+    $pdf->Ln(8);
+    $pdf->SetTextColor(0);
+    $pdf->SetFont('Arial','',10);
+    $pdf->Cell(40,10,utf8_decode('Señor(a)'));
+    $pdf->SetFont('Arial','B',10);
+    $pdf->Ln(5);
+    $pdf->Cell(40,10,utf8_decode('Verónica Tejada')); 
+    $pdf->Ln(5);
+    $pdf->Cell(40,10,utf8_decode('Ref.: Entrega de Visibilidad')); 
+    $pdf->SetFont('Arial','',10);
+    $pdf->Ln(6);
+    $pdf->Cell(40,10,utf8_decode('El trabajador (a) manifiesta que recibió dotación por parte del empleador; esta es idónea para ejecutar las labores'));
+    $pdf->Ln(5);
+    $pdf->Cell(40,10,utf8_decode('contratadas conforme a lo indicado en el artículo 230 y siguientes del Código Sustantivo del Trabajo (CST), '));
+    $pdf->Ln(5);
+    $pdf->Cell(40,10,utf8_decode('correspondiente al año 2024 así:'));
+    $pdf->SetFont('Arial','B',10);
+    $pdf->Ln(12);
+    $pdf->SetX(40);
+    $pdf->Cell(20,10,utf8_decode('CODIGO'),1,0,'C',0);
+    $pdf->Cell(45,10,utf8_decode('ARTICULO'),1,0,'C',0);
+    $pdf->Cell(70,5,utf8_decode('CANTIDAD PERIODO ENTREGADO'),1,0,'C',0);
+    $pdf->Ln(5);
+    $pdf->SetX(105);
+    $pdf->Cell(70,5,utf8_decode('abr - 27'),1,0,'C',0);
+
+    $pdf->SetFont('Arial','',10);
+    $pdf->Ln(5);
+    $pdf->SetX(40);
+    $pdf->Cell(20,10,utf8_decode('P009'),1,0,'C',0);
+    $pdf->Cell(45,10,utf8_decode('Pantalon tipo senderismo'),1,0,'C',0);
+    $pdf->Cell(70,10,utf8_decode('1'),1,0,'C',0);
+    $pdf->Ln(5);
+
+    //////////////////////////////////////////////////////////////////////////
+
+    $pdf->SetFont('Arial','',10);
+    $pdf->Ln(5);
+    $pdf->SetX(40);
+    $pdf->Cell(20,10,utf8_decode('P009'),1,0,'C',0);
+    $pdf->Cell(45,10,utf8_decode('Pantalon tipo senderismo'),1,0,'C',0);
+    $pdf->Cell(70,10,utf8_decode('1'),1,0,'C',0);
+    $pdf->Ln(5);
+
+    $pdf->SetFont('Arial','',10);
+    $pdf->Ln(5);
+    $pdf->SetX(40);
+    $pdf->Cell(20,10,utf8_decode('P009'),1,0,'C',0);
+    $pdf->Cell(45,10,utf8_decode('Pantalon tipo senderismo'),1,0,'C',0);
+    $pdf->Cell(70,10,utf8_decode('1'),1,0,'C',0);
+    $pdf->Ln(5);
+
+    ///////////////////////////////////////////////////
+
+    $pdf->Ln(12);
+    $pdf->SetFont('Arial','B',10);
+    $pdf->Cell(20,10,utf8_decode('NOTA: No se hace entrega de dinero.'),0,0,'L',0); 
+    $pdf->Ln(6);
+    $pdf->SetFont('Arial','',10);
+    $pdf->Cell(20,10,utf8_decode('Entiende y acepta que los accesorios periféricos entregadas para el desarrollo de sus funciones, y con esto la'),0,0,'L',0); 
+    $pdf->Ln(5);
+    $pdf->Cell(20,10,utf8_decode('organización cumple con la exigencia  de la solicitud.'),0,0,'L',0); 
+    $pdf->Ln(7);
+    $pdf->Cell(20,10,utf8_decode('En señal de aceptación y manifestación de recibir de conformidad, conforme a los parámetros legales establecidos, el'),0,0,'L',0); 
+    $pdf->Ln(5);
+    $pdf->Cell(20,10,utf8_decode('trabajador(a) firma el presente comprobante de entrega de accesorios.'),0,0,'L',0); 
+    $pdf->Ln(12);
+
+    $pdf->SetY(245);
+    $pdf->Cell(20,10,utf8_decode('FIRMA DE LA TRABAJADORA:'),0,0,'L',0); 
+    $pdf->SetX(110);
+    $pdf->Cell(20,10,utf8_decode('FIRMA DEL EMPLEADOR:'),0,0,'L',0); 
+    $pdf->Ln(5);
+    $pdf->Cell(20,10,utf8_decode('TIPO Y DOCUMENTO No. :'),0,0,'L',0); 
+    $pdf->SetX(110);
+    $pdf->Cell(20,10,utf8_decode('TIPO Y DOCUMENTO No. :'),0,0,'L',0); 
+    $pdf->Ln(5);
+    $pdf->Cell(20,10,utf8_decode('CARGO:'),0,0,'L',0); 
+    $pdf->SetX(110);
+    $pdf->Cell(20,10,utf8_decode('CARGO:'),0,0,'L',0); 
+
+    $pdf->Ln(5);
+    $pdf->SetX(130);
+    $pdf->Ln(5);
+    $pdf->SetX(130);
+
+    $pdf->Output();
+
+
+?>
